30eab872433d544f9bdde384ec0d74567d709d73
Yinka Bakare <delighting_minds@hotmail.com>
2024-01-26 20:04:12 +0000

    initial commit

---
+++ mywebsite/index.js
@@ -0,0 +1,9 @@
+const counter = document.querySelector(".counter-number");
+async function updateCounter() {
+let response = await fetch("https://3igea5lk7bspzrsc5frk6triim0vnuii.lambda-url.us-east-1.on.aws");
+let data = await response.json();
+counter.innerHTML = ` Views: ${data}`;
+
+}
+
+updateCounter();
